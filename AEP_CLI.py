e2b143ca0e86c7f1dcac4b8cca4451f66b14a25d
Dakoda Richardson <dakoda@Dakodas-MBP.app.byu.edu>
2020-03-10 11:02:45 -0600

    Some error fixes

--- AEP_CLI.py
+++ AEP_CLI.py
@@ -1,11 +1,13 @@
 import click
 
+
 @click.group()
 def main():
     """
     CLI tool for uploading data to Adobe Experience Platform
     """
     pass
+
 
 @main.command()
 @click.argument('filename', nargs=-1)
@@ -15,15 +17,18 @@ def upload(filename, datasetid):
     for str in filename:
         print(str)
 
+
 @main.command()
 @click.argument('config', nargs=1)
 def login(config):
     print(config)
 
+
 @main.command()
-@click.argument('batchID', nargs=1)
-def check_batch(batchID):
-    print(batchID)
+@click.argument('batchid', nargs=1)
+def check_batch(batchid):
+    print(batchid)
+
 
 @main.command()
 @click.argument('limit', nargs=1)
